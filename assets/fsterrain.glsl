2922386adfd3afacff9d32b782caaca63592d5c7
“LePeppino” <giuseppe.buccellato@hs-osnabrueck.de>
2022-03-01 17:50:39 +0100

    initial commit

---
+++ assets/fsterrain.glsl
@@ -0,0 +1,67 @@
+#version 400
+uniform vec3 EyePos;
+uniform vec3 LightPos;
+uniform vec3 LightColor;
+uniform vec3 DiffuseColor;
+uniform vec3 SpecularColor;
+uniform vec3 AmbientColor;
+uniform float SpecularExp;
+
+uniform sampler2D MixTex; // for exercise 3
+uniform sampler2D DetailTex[4]; // for exercise 3 // 4 Plätze für Zusatzaufgabe 5
+uniform vec3 Scaling;
+
+in vec3 Position;
+in vec3 Normal;
+in vec2 Texcoord0;
+//Exercise 3
+in vec2 Texcoord1;
+
+out vec4 FragColor;
+
+//Zusatzaufgabe 4
+//Visualisierung: https://thebookofshaders.com/glossary/?search=clamp
+//Formel für Nebel aus dem Aufgabenblatt, Werte und Color-Berechnung in der main unten
+float sat(in float a)
+{
+    float dmin = 0;
+    float dmax = 50;
+    
+    return clamp(pow((length(EyePos-Position)-dmin)/dmax-dmin,a), 0.0, 1.0);
+}
+
+
+void main()
+{
+    vec3 N      = normalize(Normal);
+    vec3 L      = normalize(LightPos); // light is treated as directional source
+    vec3 D      = EyePos-Position;
+    float Dist  = length(D);
+    vec3 E      = D/Dist;
+    vec3 R      = reflect(-L,N);
+    
+    vec3 DiffuseComponent = LightColor * DiffuseColor * sat(dot(N,L));
+    vec3 SpecularComponent = LightColor * SpecularColor * pow( sat(dot(R,E)), SpecularExp);
+
+   
+    // Exercise 3
+	//https://forum.openframeworks.cc/t/what-does-the-texture-function-in-glsl-do/21196
+	//https://thebookofshaders.com/glossary/?search=mix
+    vec4 color;
+    vec4 MixTextureVec = texture(MixTex,Texcoord0).rgba; 
+    //Textures
+    vec4 GrassTexture = texture(DetailTex[0], Texcoord1).rgba;
+    vec4 RockTexture = texture(DetailTex[1], Texcoord1).rgba;
+
+    //Mixe zusammen
+    color = mix(GrassTexture, RockTexture, MixTextureVec);
+   
+    //Zusatzaufgabe 4
+    float s = sat(3);
+    vec3 colorFogCalc = vec3(0.95, 0.95, 1);
+    vec4 colorFog = vec4(colorFogCalc.rgb, 1);
+    vec4 newColorFog = (1-s)*color+(s*colorFog);    
+
+    FragColor = vec4(((DiffuseComponent + AmbientColor) + SpecularComponent),1) * newColorFog; //ohne Nebel: * color;
+
+}
